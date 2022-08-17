5bff7e135cbd61d911485de6be9718feda7a6f1c
Al Camara <Alhousseinec@gmail.com>
2022-08-17 14:50:13 -0500

    update pet and pet owner object

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -6,6 +6,21 @@ using System;
 namespace pet_hotel
 {
     public enum PetBreedType {}
+
+    
     public enum PetColorType {}
-    public class Pet {}
+    public class Pet {
+        public int Id {get; set;}
+        [Required]
+        public string? Name {get; set;}
+        [Required]
+        public PetBreedType breed {get; set;}
+
+        [Required]
+        public PetColorType color {get; set;}
+        public DateTime checkedInAt {get;set;}
+        
+        [Required]
+        public int petOwnerid {get;set;}
+    }
 }
